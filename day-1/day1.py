bd851333da7579668f7c7cdaccf1254a6de93d60
Jasson <jassonjasson17@gmail.com>
2025-07-01 12:05:36 +0700

    Day 1

---
+++ day-1/day1.py
@@ -0,0 +1,14 @@
+# Printing
+print("Hello World")
+
+# String Manipulation
+print("Hello World\nHello World\nHello World")
+
+# Concatenate
+print("Hello" + " " + "Jasson")
+
+# Input Function
+input("What is your name? ")
+
+# Variables
+name = input("Whats your name? ")
